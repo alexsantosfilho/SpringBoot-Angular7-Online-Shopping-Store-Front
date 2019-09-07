62b999cb2b8cf9e2a9113abf41b5f045b94c9185
alexsantosfilho <devalexsandro@gmail.com>
2019-09-07 12:38:36 -0300

    remove '/'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const apiUrl = '//back.tudomaistv.com:8080//api';
+export const apiUrl = '//back.tudomaistv.com:8080/api';
 
 /*
  * For easier debugging in development mode, you can import the following file
